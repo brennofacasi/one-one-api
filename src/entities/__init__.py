8ce37a1b96b9a8ae299c31e629805a94b5e46cde
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-09 19:30:48 -0300

    fix: added entities to init

---
+++ src/entities/__init__.py
@@ -0,0 +1,5 @@
+from src.entities.meeting import Meeting
+from src.entities.history import History
+from src.entities.mentee import Mentee
+from src.entities.mentor import Mentor
+from src.entities.minute import Minute
