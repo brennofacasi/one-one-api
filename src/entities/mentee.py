e2f963d0f7bf9a478b391b9ab1628aa988d4d8ae
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-10 18:56:53 -0300

    fix: added first and last names

--- src/entities/mentee.py
+++ src/entities/mentee.py
@@ -1,5 +1,6 @@
 class Mentee:
-    def __init__(self, name, email, company):
-        self.name = name
+    def __init__(self, first_name, last_name, email, company):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.company = company
