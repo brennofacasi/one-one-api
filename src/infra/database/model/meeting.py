83a346b8bed9d01f583aa49935428ab41dd70db6
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-10 18:57:15 -0300

    fix: replaced uuid to string

--- src/infra/database/model/meeting.py
+++ src/infra/database/model/meeting.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, UUID,  DateTime, Integer, ForeignKey
+from sqlalchemy import Column, String,  DateTime, Integer, ForeignKey
 from . import Base
 
 
@@ -8,14 +8,17 @@ class Meeting(Base):
     ''' Meeting Database Model '''
     __tablename__ = 'meeting'
 
-    id = Column('id', UUID, primary_key=True)
+    id = Column('id', String, primary_key=True)
     date = Column(DateTime, nullable=False)
     mentor_id = Column(Integer, ForeignKey('mentor.id'), nullable=False)
     mentee_id = Column(Integer, nullable=False)
     duration = Column(Integer)
     created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime)
 
-    mentor = relationship('Mentor')
+    # mentor = relationship('Mentor')
+    history = relationship('History')
+    minute = relationship('Minute')
 
     def __repr__(self) -> str:
         return f'Meeting(id={self.id!r}, date={self.date!r})'
--- src/infra/database/model/mentor.py
+++ src/infra/database/model/mentor.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, UUID, String, DateTime
+from sqlalchemy import Column, String, DateTime
 from . import Base
 
 
@@ -8,12 +8,14 @@ class Mentor(Base):
     ''' Mentor Database Model '''
     __tablename__ = 'mentor'
 
-    id = Column('id', UUID, primary_key=True)
-    name = Column(String, nullable=False)
+    id = Column('id', String, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String)
     email = Column(String)
     created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime)
 
     meeting = relationship('Meeting')
 
     def __repr__(self) -> str:
-        return f'Mentor(id={self.id!r}, name={self.name!r}, email={self.email!r})'
+        return f'Mentor(id={self.id!r}, name={self.first_name!r}, email={self.email!r})'
