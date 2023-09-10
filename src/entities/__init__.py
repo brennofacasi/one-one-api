929da9839b5d8d3cc16f6148bb15357331d62b90
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-09 21:51:44 -0300

    feat: new status entity

--- src/entities/__init__.py
+++ src/entities/__init__.py
@@ -3,3 +3,4 @@ from src.entities.history import History
 from src.entities.mentee import Mentee
 from src.entities.mentor import Mentor
 from src.entities.minute import Minute
+from src.entities.status import Status
