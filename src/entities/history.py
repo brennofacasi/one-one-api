56a308bf866cb3d653314dcb324678e78c4f615b
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-09 21:51:55 -0300

    fix: removed status options

--- src/entities/history.py
+++ src/entities/history.py
@@ -1,14 +1,5 @@
 class History:
-    def __init__(self, meeting, status, created_at):
+    def __init__(self, meeting, created_at, status='SCHEDULED'):
         self.meeting = meeting
-        self.status = status
         self.created_at = created_at
-
-    def done(self):
-        self.status = 'DONE'
-
-    def rescheduled(self):
-        self.status = 'RESCHEDULED'
-
-    def cancelled(self):
-        self.status = 'CANCELLED'
+        self.status = status
