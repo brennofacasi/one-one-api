487309e996330579c4e70a118737678bbea30a43
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-11 20:21:31 -0300

    feat: added db model

--- src/infra/repositories/flask_meeting_repository.py
+++ src/infra/repositories/flask_meeting_repository.py
@@ -1,5 +1,6 @@
 from src.usecases.ports import MeetingRepository
 from src.infra.database import Session
+from src.infra.database.model import MeetingModel
 
 
 class FlaskMeetingRepository(MeetingRepository):
@@ -7,11 +8,14 @@ class FlaskMeetingRepository(MeetingRepository):
         self.session = Session()
 
     def add(self, meeting):
-        self.session.add(meeting)
+        meeting_model = MeetingModel(
+            meeting.date, meeting.mentor, meeting.mentee, meeting.duration, meeting.kind)
+        self.session.add(meeting_model)
         self.session.commit()
         return {
             "message": "Meeting added successfully."
         }
 
     def find_by_id(self, meeting_id):
+        # TO DO - Implement Find by Id
         return super().find_by_id(meeting_id)
