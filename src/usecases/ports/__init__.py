1adc51c121859d3eacad66b19008c0154c94f727
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-10 22:05:49 -0300

    feat: updated imports

--- src/usecases/ports/__init__.py
+++ src/usecases/ports/__init__.py
@@ -1,3 +1,4 @@
 from .meeting_repository import MeetingRepository
 from .history_repository import HistoryRepository
 from .minute_repository import MinuteRepository
+from .mentor_repository import MentorRepository
