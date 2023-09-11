746f39bd8d0de2c6fd24e7dfe3a9ed97e40eb49a
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-10 22:04:41 -0300

    feat: meeting schemas

---
+++ src/infra/schemas/meeting.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from datetime import datetime
+from src.entities import Status
+
+
+class MeetingSchema(BaseModel):
+    id: str
+    date: datetime
+    mentor_id: int
+    mentee_id: int
+    duration: int
+    kind: Status
+
+
+class MeetingViewSchema(BaseModel):
+    id: str
+    date: datetime
+    mentor_id: int
+    mentee_id: int
+    duration: int
+    kind: str
+    created_at: datetime
+    updated_at: datetime
