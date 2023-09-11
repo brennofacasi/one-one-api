cc30a9a80c74fdfba290335e2fb7c35e4a55f8bd
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-10 22:04:54 -0300

    fix: minor fixes

--- src/usecases/meeting/create_meeting.py
+++ src/usecases/meeting/create_meeting.py
@@ -5,7 +5,7 @@ class CreateMeeting:
     def __init__(self, meeting_repository):
         self.meeting_repository = meeting_repository
 
-    def perform(self, id, mentor, mentee, date, duration, kind):
+    def execute(self, mentor, mentee, date, duration, kind, id):
         repository = self.meeting_repository
-        meeting = Meeting(id, mentor, mentee, date, duration, kind)
+        meeting = Meeting(mentor, mentee, date, duration, kind, id)
         repository.add(meeting)
