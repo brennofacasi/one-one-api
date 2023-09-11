36257b4f2752d324d1c2188b6712b5a4e193a56d
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-11 20:24:25 -0300

    feat: added duplicate error

--- src/usecases/mentor/create_mentor.py
+++ src/usecases/mentor/create_mentor.py
@@ -1,11 +1,16 @@
 from src.entities import Mentor
+from .errors import DuplicateMentorError
 
 
 class CreateMentor:
     def __init__(self, mentor_repository):
         self.mentor_repository = mentor_repository
 
-    def execute(self, first_name, last_name, email):
+    def execute(self, mentor):
         repository = self.mentor_repository
-        mentor = Mentor(first_name, last_name, email)
+
+        if repository.find_by_email(mentor.email) != None:
+            raise DuplicateMentorError()
+
+        mentor = Mentor(mentor.first_name, mentor.last_name, mentor.email)
         repository.add(mentor)
