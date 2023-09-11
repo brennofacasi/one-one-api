8286338eb5a8c643b6afbb58703c7869736a44b1
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-11 20:26:42 -0300

    fix: minor fixes

--- tests/create_mentor_test.py
+++ tests/create_mentor_test.py
@@ -1,11 +1,11 @@
+from src.entities import Mentor
 from src.usecases.mentor import CreateMentor
 from .repositories.in_memory_mentor_repository import InMemoryMentorRepository
 
 
 def test_create_mentor_in_memory():
     mentor_repository = InMemoryMentorRepository()
-    first_name = 'Brenno'
-    last_name = 'Cavalcante'
-    email = 'brenno@nouhau.pro'
-    CreateMentor(mentor_repository).execute(first_name, last_name, email)
-    assert mentor_repository.find_by_email(email).email == email
+    mentor = Mentor('Brenno', 'Cavalcante', 'brenno@nouhau.pro')
+    usecase = CreateMentor(mentor_repository)
+    usecase.execute(mentor)
+    assert mentor_repository.find_by_email(mentor.email).email == mentor.email
