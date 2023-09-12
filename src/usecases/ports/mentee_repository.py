848d67c5d485b101494040430dc78fbd23f68908
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-12 11:37:41 -0300

    feat: get mentees rep

---
+++ src/usecases/ports/mentee_repository.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class MenteeRepository(ABC):
+    @abstractmethod
+    def get_all(self):
+        raise (NotImplementedError)
+
+    @abstractmethod
+    def find_by_email(self, email):
+        raise (NotImplementedError)
+
+    @abstractmethod
+    def find_by_id(self, id):
+        raise (NotImplementedError)
