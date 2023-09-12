5cde2ef128cec14da355d39b7b9e9ff2682bf3d1
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-12 14:29:59 -0300

    feat: added nanoid

--- tests/create_meeting_test.py
+++ tests/create_meeting_test.py
@@ -1,4 +1,4 @@
-import uuid
+from nanoid import generate
 from src.entities import Mentor, Mentee, Meeting
 from tests.repositories.in_memory_meeting_repository import InMemoryMeetingRepository
 from src.usecases.meeting import CreateMeeting
@@ -6,11 +6,10 @@ from src.usecases.meeting import CreateMeeting
 
 def test_create_meeting():
     meeting_repository = InMemoryMeetingRepository()
-    id = uuid.uuid1()
     mentor = Mentor('João', 'Moura', 'joao@email.com')
     mentee = Mentee('Ana', 'Ribeiro', 'ana@email.com', 'Nouhau')
     meeting = Meeting(
-        mentor, mentee, '2023-09-09 19:01:38.232448', 30, 'ONLINE', id)
-    CreateMeeting(meeting_repository).execute(
+        mentor, mentee, '2023-09-09 19:01:38.232448', 30, 'ONLINE')
+    id = CreateMeeting(meeting_repository).execute(
         meeting)
     assert meeting_repository.find_by_id(id).mentor == mentor
