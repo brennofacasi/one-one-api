e6fafab688b76907ad32153a1a7acd3af60e45a4
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-12 11:36:22 -0300

    feat: added toJson func

--- src/entities/mentee.py
+++ src/entities/mentee.py
@@ -1,6 +1,18 @@
+import json
+
+
 class Mentee:
     def __init__(self, first_name, last_name, email, company):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.company = company
+
+    def toJSON(self, id):
+        return {
+            "id": id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "company": self.company,
+        }
