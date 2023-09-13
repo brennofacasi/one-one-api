d02a75332e76e517e2d62a9d9a3584aa39aa6227
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-13 15:00:07 -0300

    feat: added check if mentor and mentee exist

--- src/usecases/meeting/create_meeting.py
+++ src/usecases/meeting/create_meeting.py
@@ -1,15 +1,45 @@
 from src.entities import Meeting
-from nanoid import generate
+from src.usecases.ports import MeetingRepository, MentorRepository, MenteeRepository, GenerateServices
+from .errors import MentorDoesNotExistError, MenteeDoesNotExistError
 
 
 class CreateMeeting:
-    def __init__(self, meeting_repository):
-        self.meeting_repository = meeting_repository
+    '''Construct a new :class:`CreateMeeting`.
 
-    def execute(self, meeting):
-        repository = self.meeting_repository
-        id = generate()
-        meeting = Meeting(meeting.mentor, meeting.mentee,
+    It is used to create a new meeting in the repository.
+    The class requires two arguments:
+
+    `meeting_repository` The repository class.
+
+    `generator` The generator class, used to create ids and other utils.
+
+    '''
+
+    def __init__(self, meeting_repository: MeetingRepository, mentor_repository: MentorRepository, mentee_repository: MenteeRepository, generator: GenerateServices):
+        self.meeting_repository = meeting_repository
+        self.mentor_repository = mentor_repository
+        self.mentee_repository = mentee_repository
+
+        self.generator = generator
+        self.id = generator.id()
+
+    def execute(self, meeting: Meeting):
+        '''
+        It executes the creation of a meeting. It requires the ```meeting``` object.
+        '''
+        mentor_repository = self.mentor_repository
+        mentee_repository = self.mentee_repository
+
+        if mentor_repository.find_by_id(meeting.mentor_id) == None:
+            raise MentorDoesNotExistError()
+
+        if mentee_repository.find_by_id(meeting.mentee_id) == None:
+            raise MenteeDoesNotExistError()
+
+        id = self.id
+        meeting_repository = self.meeting_repository
+        content = Meeting(meeting.mentor_id, meeting.mentee_id,
                           meeting.date, meeting.duration, meeting.kind, id)
-        repository.add(meeting)
+
+        meeting_repository.add(content)
         return id
