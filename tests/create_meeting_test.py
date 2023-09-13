a803b501035e52d29a0d2e192dcb944a3518b4c9
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-13 16:58:59 -0300

    fix: minor fixes

--- tests/create_meeting_test.py
+++ tests/create_meeting_test.py
@@ -1,16 +1,28 @@
 
-from src.entities import Mentor, Mentee, Meeting
+from src.entities import Mentor, Meeting
 from tests.repositories.fake_generate_services import FakeGenerate
 from tests.repositories.in_memory_meeting_repository import InMemoryMeetingRepository
+from tests.repositories.in_memory_mentor_repository import InMemoryMentorRepository
+from tests.repositories.in_memory_mentee_repository import InMemoryMenteeRepository
+
 from src.usecases.meeting import CreateMeeting
 
 
 def test_create_meeting():
     meeting_repository = InMemoryMeetingRepository()
+    mentor_repository = InMemoryMentorRepository()
+    mentee_repository = InMemoryMenteeRepository()
     generator = FakeGenerate()
+
     mentor = Mentor('João', 'Moura', 'joao@email.com', 2)
-    mentee = Mentee('Ana', 'Ribeiro', 'ana@email.com', 'Nouhau', 3)
+    mentor_repository.add(mentor)
+
+    mentee_id = 2
+
     meeting = Meeting(
-        mentor.id, mentee.id, '2023-09-09 19:01:38.232448', 30, 'ONLINE')
-    id = CreateMeeting(meeting_repository, generator).execute(meeting)
+        mentor.id, mentee_id, '2023-09-09 19:01:38.232448', 30, 'ONLINE')
+
+    id = CreateMeeting(meeting_repository, mentor_repository,
+                       mentee_repository, generator).execute(meeting)
+
     assert meeting_repository.find_by_id(id).mentor_id == mentor.id
