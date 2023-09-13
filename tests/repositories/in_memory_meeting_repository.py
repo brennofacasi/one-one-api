8fc9fb2e4925b473db5e10cb89a4e71b00e598f5
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-13 15:01:33 -0300

    feat: get all def in memory

--- tests/repositories/in_memory_meeting_repository.py
+++ tests/repositories/in_memory_meeting_repository.py
@@ -12,3 +12,6 @@ class InMemoryMeetingRepository(MeetingRepository):
         for meeting in self.meetings:
             if meeting.id == meeting_id:
                 return meeting
+
+    def get_all(self):
+        return self.meetings
