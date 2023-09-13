de7b800b75872ca049e6a8f129dbd6c279fb0c91
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-13 14:29:24 -0300

    feat: show meetings function

--- src/infra/schemas/meeting.py
+++ src/infra/schemas/meeting.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import List
 
 
 class MeetingSchema(BaseModel):
     date: datetime
-    mentor: int
-    mentee: int
+    mentor_id: int
+    mentee_id: int
     duration: int
     kind: str
 
@@ -13,9 +14,29 @@ class MeetingSchema(BaseModel):
 class MeetingViewSchema(BaseModel):
     id: str
     date: datetime
-    mentor_id: int
-    mentee_id: int
+    mentor: object
+    mentee_id: str
     duration: int
     kind: str
     created_at: datetime
     updated_at: datetime
+
+
+def show_meetings(meetings: List[MeetingViewSchema], mentee_repository):
+    result = []
+    for meeting in meetings:
+        mentee = mentee_repository.find_by_id(meeting.mentee_id)
+        result.append({
+            "id": meeting.id,
+            "date": meeting.date,
+            "mentor": {
+                "first_name": meeting.mentor.first_name,
+                "last_name": meeting.mentor.last_name,
+            },
+            "mentee": mentee,
+            "duration": meeting.duration,
+            "kind": meeting.kind,
+            "created_at": meeting.created_at,
+            "updated_at": meeting.updated_at
+        })
+    return result
