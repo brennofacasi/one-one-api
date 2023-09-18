b384a44fb30f7d5b682534c0712e39c843a489e8
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-17 23:51:41 -0300

    fix: minor fixes

--- src/infra/blueprints/mentee_bp.py
+++ src/infra/blueprints/mentee_bp.py
@@ -3,19 +3,19 @@ from src.infra.schemas import MenteeViewSchema
 from src.infra.repositories import ApiMenteeRepository
 from src.usecases.mentees import GetMentees
 
-mentee_blueprint = APIBlueprint('mentee', __name__, url_prefix='/mentee')
+mentee_blueprint = APIBlueprint("mentee", __name__, url_prefix="/mentee")
 
-mentee_tag = Tag(name='Mentodaros',
-                 description='Adição, visualização e atualização de usuários mentorados.')
+mentee_tag = Tag(name="Mentodaros",
+                 description="Adição, visualização e atualização de usuários mentorados.")
 
 
-@mentee_blueprint.get('/', tags=[mentee_tag], responses={'200': MenteeViewSchema})
+@mentee_blueprint.get("/", tags=[mentee_tag], responses={"200": MenteeViewSchema})
 def get_mentees():
-    '''
+    """
     Mostra usuários mentorados a partir de API Externa
 
     Retorna representação da listagem de todos os usuários que podem participar de mentorias.
-    '''
+    """
     repository = ApiMenteeRepository()
     result = GetMentees(repository).execute()
     return result
