262e192486bc9de68913fe3166c69605a417588d
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-17 23:51:07 -0300

    feat: added erros and organized code

--- src/infra/blueprints/meeting_bp.py
+++ src/infra/blueprints/meeting_bp.py
@@ -1,26 +1,27 @@
 from flask_openapi3 import Tag, APIBlueprint
-
-from src.usecases.meeting import CreateMeeting, GetMeetings, DeleteMeeting
-from src.usecases.meeting.errors import MentorDoesNotExistError, MenteeDoesNotExistError, MeetingNotFoundError
-
+from src.entities.meeting import Meeting
 from src.infra.services import Generate
-from src.infra.schemas import MeetingViewSchema, MeetingSchema, ErrorSchema, SuccessSchema, MeetingSearchById, show_meetings
-from src.infra.repositories import DBMeetingRepository, DBMentorRepository, ApiMenteeRepository
+from src.infra.schemas import ErrorSchema, SuccessSchema
+
+from src.usecases.meeting import *
+from src.usecases.meeting.errors import *
+from src.infra.schemas.meeting import *
+from src.infra.repositories import *
 
 meeting_tag = Tag(
-    name='Reuniões', description='Adição, visualização, atualização e deleção de reuniões.')
+    name="Reuniões", description="Adição, visualização, atualização e deleção de reuniões.")
 
 meeting_blueprint = APIBlueprint(
-    'meeting', __name__, url_prefix='/meeting', abp_tags=[meeting_tag])
+    "meeting", __name__, url_prefix="/meeting", abp_tags=[meeting_tag])
 
 
-@meeting_blueprint.get('/', responses={"200": MeetingViewSchema, "404": ErrorSchema})
+@meeting_blueprint.get("/", responses={"200": MeetingViewSchema, "404": ErrorSchema})
 def get_meetings():
-    '''
+    """
     Mostra todas as reuniões.
 
     Retorna representação da listagem de todas as reuniões cadastradas no banco de dados.
-    '''
+    """
     meeting_repository = DBMeetingRepository()
     mentee_repository = ApiMenteeRepository()
 
@@ -28,46 +29,51 @@ def get_meetings():
     return show_meetings(result, mentee_repository)
 
 
-@meeting_blueprint.post('/', responses={"200": SuccessSchema, "400": ErrorSchema})
+@meeting_blueprint.post("/", responses={"200": SuccessSchema, "400": ErrorSchema})
 def add_meeting(body: MeetingSchema):
-    '''
+    """
     Adiciona reunião ao banco de dados.
 
     Retorna mensagem de sucesso com o id.
-    '''
+    """
 
     meeting_repository = DBMeetingRepository()
     mentor_repository = DBMentorRepository()
     mentee_repository = ApiMenteeRepository()
+    slot_repository = DBSlotRepository()
     generator = Generate()
 
     try:
-
-        id = CreateMeeting(meeting_repository,
-                           mentor_repository, mentee_repository, generator).execute(body)
+        meeting = Meeting(body.mentor_id, body.mentee_id,
+                          body.slot_id, body.kind)
+        meeting_id = CreateMeeting(meeting_repository, mentor_repository,
+                                   mentee_repository, slot_repository, generator).execute(meeting)
         return {
-            "id": id,
+            "id": meeting_id,
             "message": "Meeting added successfully."
         }, 200
 
     except MentorDoesNotExistError as e:
-        return {
-            "message": "This mentor does not exist."
-        }, 400
+        return {"message": str(e)}, 400
 
     except MenteeDoesNotExistError as e:
-        return {
-            "message": "This mentee does not exist."
-        }, 400
+        return {"message": str(e)}, 400
+
+    except SlotUnavailable as e:
+        return {"message": str(e)}, 400
+
+    except Exception as e:
+        print(str(e))
+        return {"message": "Oops, there's something wrong."}, 400
 
 
-@meeting_blueprint.delete('/<string:id>', responses={"200": SuccessSchema, "400": ErrorSchema})
+@meeting_blueprint.delete("/<string:id>", responses={"200": SuccessSchema, "400": ErrorSchema})
 def delete_meeting(path: MeetingSearchById):
-    '''
+    """
     Deleta reunião do bando de dados.
 
     Realiza a deleção da reunião pelo id. Retorna mensagem de sucesso.
-    '''
+    """
     meeting_repository = DBMeetingRepository()
     try:
 
@@ -78,6 +84,4 @@ def delete_meeting(path: MeetingSearchById):
         }, 200
 
     except MeetingNotFoundError as e:
-        return {
-            "message": "Meeting not found."
-        }
+        return {"message": str(e)}, 404
