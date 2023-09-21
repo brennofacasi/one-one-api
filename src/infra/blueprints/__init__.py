1424a51ecd433d07f6e8092c7cfbe355dbfc8d04
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-21 16:48:12 -0300

    feat: add availability bp

--- src/infra/blueprints/__init__.py
+++ src/infra/blueprints/__init__.py
@@ -2,3 +2,4 @@ from .meeting_bp import meeting_blueprint
 from .mentor_bp import mentor_blueprint
 from .mentee_bp import mentee_blueprint
 from .slot_bp import slot_blueprint
+from .availability_bp import availability_blueprint
