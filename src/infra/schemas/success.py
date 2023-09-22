363f72c2db06b89a1a28e3ae5e9ff9d6c485e822
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-22 14:11:26 -0300

    feat: added schema with id

--- src/infra/schemas/success.py
+++ src/infra/schemas/success.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
 
 
-class SuccessSchema(BaseModel):
+class SuccessSchemaWithId(BaseModel):
     """ Define como uma mensagem de sucesso será representada
     """
     id: str
+    message: str
+
+
+class SuccessSchema(BaseModel):
+    """ Define como uma mensagem de sucesso será representada
+    """
     message: str
