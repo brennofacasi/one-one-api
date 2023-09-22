c969ad9d4b76cb458db5acdfb08994811488df1f
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-22 00:00:33 -0300

    refact: removed is available, minor fixes and unique constrains

--- src/infra/database/model/slot.py
+++ src/infra/database/model/slot.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Union
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Boolean,  DateTime, Integer, ForeignKey
+from sqlalchemy import Column,  DateTime, Integer, ForeignKey, UniqueConstraint, String
 
 from . import Base
 from src.entities import Slot
@@ -11,13 +11,12 @@ class SlotModel(Base):
     """ Slot Database Model """
     __tablename__ = "slot"
 
-    id = Column("id", Integer, primary_key=True)
+    id = Column("id", String, primary_key=True)
     mentor_id = Column(Integer, ForeignKey("mentor.id"), nullable=False)
     start_time = Column(DateTime, nullable=False)
     end_time = Column(DateTime, nullable=False)
-    is_available = Column(Boolean, nullable=False, default=True)
 
-    created_at = Column(DateTime, default=datetime.now())
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
     updated_at = Column(DateTime)
 
     mentor = relationship("MentorModel", foreign_keys=[
@@ -25,13 +24,15 @@ class SlotModel(Base):
 
     meeting = relationship("MeetingModel", back_populates="slot")
 
+    __table_args__ = (UniqueConstraint('mentor_id', 'start_time', 'end_time'),)
+
     def __init__(self, slot: Slot, created_at: Union[DateTime, None] = None,
                  updated_at: Union[DateTime, None] = None):
 
         self.mentor_id = slot.mentor_id
         self.start_time = slot.start_time
         self.end_time = slot.end_time
-        self.is_available = slot.is_available
+        self.id = slot.id
 
         if created_at:
             self.created_at = created_at
