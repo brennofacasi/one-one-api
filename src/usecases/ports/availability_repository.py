ebca9496c6f1f76ac89a34f833761bdfd437f123
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-21 23:26:13 -0300

    feat: new add many, find by id and by mentor id

--- src/usecases/ports/availability_repository.py
+++ src/usecases/ports/availability_repository.py
@@ -3,9 +3,17 @@ from abc import ABC, abstractmethod
 
 class AvailabilityRepository(ABC):
     @abstractmethod
-    def add(self):
+    def add(self, availability):
         raise NotImplementedError
 
     @abstractmethod
-    def find_by_mentor_id(self):
+    def add_many(self, availabilities):
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_id(self, availability_id):
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_mentor_id(self, mentor_id):
         raise NotImplementedError
