a4d3cafa75973fa264d691e4355f0bb3ee0c6bdd
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-21 23:43:28 -0300

    refact: deleted patch and aded delete

--- src/infra/blueprints/slot_bp.py
+++ src/infra/blueprints/slot_bp.py
@@ -1,11 +1,8 @@
 from flask_openapi3 import Tag, APIBlueprint
-from src.entities import Slot
 
-from src.infra.schemas import SlotSchema, SlotSearchById
-from src.infra.repositories import DBSlotRepository
-from src.usecases.slot import CreateSlot
-
-from src.infra.services import Generate
+from src.infra.schemas import SlotSearchById, SuccessSchema, ErrorSchema
+from src.infra.repositories import DBSlotRepository, DBMentorRepository
+from src.usecases.slot import DeleteSlot
 
 
 slot_tag = Tag(
@@ -15,21 +12,13 @@ slot_blueprint = APIBlueprint(
     "slot", __name__, url_prefix="/slot", abp_tags=[slot_tag])
 
 
-@slot_blueprint.post("/")
-def add_slot(body: SlotSchema):
+@slot_blueprint.delete("/<string:id>", responses={"200": SuccessSchema, "400": ErrorSchema})
+def delete_slot(path: SlotSearchById):
     slot_repository = DBSlotRepository()
-    generate_service = Generate()
 
-    slot = Slot(body.mentor_id, body.start_time,
-                body.end_time, body.is_available)
-    CreateSlot(generate_service, slot_repository).execute(slot)
+    try:
+        DeleteSlot(slot_repository).execute(path.id)
+        return {"message": "Slot deleted."}, 200
 
-    return {"message": "Slot criado"}, 200
-
-
-@slot_blueprint.patch("/<int:id>")
-def update_slot(path: SlotSearchById):
-    slot_repository = DBSlotRepository()
-    slot_repository.set_unavailable_by_id(path.id)
-
-    return {"message": "Foi?"}, 200
+    except Exception as e:
+        return {"message": str(e)}, 400
