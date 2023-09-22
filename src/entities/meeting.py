9f2e1d8c0b03da568ae7ddcc416a44b98c31a0a5
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-21 23:31:34 -0300

    fix: minor fixes

--- src/entities/meeting.py
+++ src/entities/meeting.py
@@ -1,11 +1,8 @@
 
 class Meeting:
-    def __init__(self, mentor_id, mentee_id, slot_id, kind, id=None):
+    def __init__(self, mentor_id, mentee_id, slot, kind, id=None):
         self.mentor_id = mentor_id
         self.mentee_id = mentee_id
-        self.slot_id = slot_id
+        self.slot = slot
         self.kind = kind
-        self.id = id
-
-    def set_id(self, id):
         self.id = id
--- src/entities/slot.py
+++ src/entities/slot.py
@@ -1,10 +1,6 @@
 class Slot:
-    def __init__(self, mentor_id, start_time, end_time, is_available=True, id=None):
+    def __init__(self, mentor_id, start_time, end_time, id=None):
         self.mentor_id = mentor_id
         self.start_time = start_time
         self.end_time = end_time
-        self.is_available = is_available
-        self.id = id
-
-    def set_id(self, id):
         self.id = id
