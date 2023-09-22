497348794a0dbb606ddd6574a4e878e659391ddc
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-22 14:12:29 -0300

    feat: added availability bp

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask_cors import CORS
 from flask import redirect
+from flask_cors import CORS
 from flask_openapi3 import Info, OpenAPI, Tag
-from src.infra.blueprints import meeting_blueprint, mentor_blueprint, mentee_blueprint, slot_blueprint
+from src.infra.blueprints import *
 
-info = Info(title="Mementor Api",
-            description="Aplicação para agendamento de reuniões de mentorias.", version="1.0.0")
+# -----------------------
+title = "MentorApi"
+description = "Aplicação para agendamento de reuniões de mentorias."
+version = "1.0.0"
+# -----------------------
+
+info = Info(title=title, description=description, version=version)
 app = OpenAPI(__name__, info=info)
 
 CORS(app)
@@ -23,3 +28,4 @@ app.register_api(meeting_blueprint)
 app.register_api(mentor_blueprint)
 app.register_api(mentee_blueprint)
 app.register_api(slot_blueprint)
+app.register_api(availability_blueprint)
