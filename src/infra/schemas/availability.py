d14d5cea08d01fe00f599fb644eb34eb0e4c51e0
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-22 14:03:01 -0300

    feat: added create schema list

--- src/infra/schemas/availability.py
+++ src/infra/schemas/availability.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
-from datetime import datetime
-from typing import List
+from datetime import time
 
 
 class AvailabilitySchema(BaseModel):
-    mentor_id: int
-    week_day: int
-    from_time: datetime
-    to_time: datetime
+    mentor_id: int = 1
+    week_day: int = 0
+    from_time: time = "10:00:00"
+    to_time: time = "15:00:00"
+
+
+class AvailabilityCreateSchema(BaseModel):
+    availabilities: list[AvailabilitySchema]
