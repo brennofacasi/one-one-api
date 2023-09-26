baa908e0fab34befc514957391df5a1b39765038
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 20:02:09 -0300

    feat: added meeting id

--- src/entities/slot.py
+++ src/entities/slot.py
@@ -1,6 +1,7 @@
 class Slot:
-    def __init__(self, mentor_id, start_time, end_time, id=None):
+    def __init__(self, mentor_id, start_time, end_time, meeting_id=None, id=None):
         self.mentor_id = mentor_id
+        self.meeting_id = meeting_id
         self.start_time = start_time
         self.end_time = end_time
         self.id = id
