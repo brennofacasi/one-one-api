23c04be1e9e140adb05fb2a68fd16b740883e24f
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-26 20:05:07 -0300

    del: deleted old tests

--- tests/find_availability_test.py
+++
@@ -1,7 +0,0 @@
-from tests.repositories.in_memory_availability_repository import InMemoryAvailabilityRepository
-
-
-def test_find_availability_in_memory():
-    availability_repository = InMemoryAvailabilityRepository()
-    found_availability = availability_repository.find_by_mentor_id(5)
-    assert len(found_availability) == 4
--- tests/repositories/fake_generate_services.py
+++
@@ -1,8 +0,0 @@
-import random
-from src.infra.services import Generate
-
-
-class FakeGenerate(Generate):
-    def id(self):
-        generatedId = f"id_{random.randint(5, 200)}"
-        return generatedId
