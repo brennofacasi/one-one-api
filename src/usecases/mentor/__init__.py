27da204cc6eb5e99002937b789370ed3a18c1bb0
Brenno Cavalcante <brennofaby@gmail.com>
2023-09-27 10:40:38 -0300

    feat: added edit mentor use case

--- src/usecases/mentor/__init__.py
+++ src/usecases/mentor/__init__.py
@@ -2,3 +2,4 @@ from .create_mentor import CreateMentor
 from .get_mentors import GetMentors
 from .get_mentor_available_slots import GetMentorAvailableSlots
 from .delete_mentor import DeleteMentor
+from .edit_mentor import EditMentor
